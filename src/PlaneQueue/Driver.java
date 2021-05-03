9a1f1c7f6b840656954a31e862152592e1d6a6e0
dhillonj2 <60679811+dhillonj2@users.noreply.github.com>
2021-05-02 20:51:05 -0400

    added busy time
    Will now display, at the bottom of the output, how long the server has been busy for

--- src/PlaneQueue/Driver.java
+++ src/PlaneQueue/Driver.java
@@ -6,9 +6,12 @@ public class Driver {
         // In seconds
         final double avgArrivalTime = 146.2;
         final double avgServiceTime = 42.0;
+        double busyTime = 0.0; //running number of how long the server is busy for
         PlaneQueue sq = new PlaneQueue(avgServiceTime, avgArrivalTime, 30, 5);
         System.out.printf("%12s %12s %12s", "Plane Callsign", "Entry Time", "Service Time\n");
         for(Plane p: sq.planes)
             System.out.println(p.toString());
     }
+    String tempBusy  = String.format("%12.3f", busyTime);
+    System.out.printf("Total Busy Time: " + tempBusy);
 }
