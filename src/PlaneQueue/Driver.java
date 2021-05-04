a621a647c2c79cf64d6bf44c41635506ef29050e
dhillonj2 <60679811+dhillonj2@users.noreply.github.com>
2021-05-04 14:05:43 -0400

    Added u-hat and total simulation time
    Completed the u-hat calculations and cleaned up the output

--- src/PlaneQueue/Driver.java
+++ src/PlaneQueue/Driver.java
@@ -7,13 +7,32 @@ public class Driver {
         final double avgArrivalTime = 146.2;
         final double avgServiceTime = 42.0;
         double busyTime = 0.0; //running number of how long the server is busy for
+        double uHat = 0.0;
+        double totalSim = 0.0; //total time of simulation
+        double lastService = 0.0; 
         PlaneQueue sq = new PlaneQueue(avgServiceTime, avgArrivalTime, 30, 5);
         System.out.printf("%12s %12s %12s", "Plane Callsign", "Entry Time", "Service Time\n");
         for(Plane p: sq.planes) {
             System.out.println(p.toString());
             busyTime = busyTime + p.getServiceTime();
+            totalSim = p.getEntryTime();
+            lastService = p.getServiceTime();
         }
-        String tempBusy  = String.format("%12.3f", busyTime);
-        System.out.print("Total Busy Time: " + tempBusy);
+        
+        //Calculations for u-hat
+        totalSim += lastService;
+        uHat = busyTime / totalSim;
+        uHat *= 100;
+        
+        
+        String tempUHat  = String.format("%7.2f", uHat);
+        System.out.printf("u-hat : " + tempUHat + "%% \n");
+        
+        String tempBusy  = String.format("%9.2f", busyTime);
+        System.out.printf("B(n)  : " + tempBusy + "\n");
+        
+        String tempTotalSim  = String.format("%7.2f", totalSim);
+        System.out.printf("Total Simulation Time : " + tempTotalSim + "\n");
+
     }
 }
